cc0d7f37e9b73706aa0ec1441258e613097aa66c
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-01-18 22:18:57 +0530

    removed axios & using fetch

--- app/page.js
+++ app/page.js
@@ -1,16 +1,26 @@
 "use client";
 import Prompt from "@/data/Prompt";
-import axios from "axios";
+// import axios from "axios";
 
 export default function Home() {
   const messages = { content: "make a new app", role: "user" };
 
   const handleSubmit = async () => {
     const aiPrompt = messages.content + " " + Prompt.CODE_GEN_PROMPT;
-    const fetch = await axios.post("api/ai-code", {
+    // const fetch = await axios.post("api/ai-code", {
+    //   prompt: aiPrompt,
+    // });
+    const prompt = {
       prompt: aiPrompt,
+    };
+    const res = await fetch("api/ai-code", {
+      method: "POST",
+      // method: "POST",
+      body: JSON.stringify(prompt),
     });
-    console.log(fetch);
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
   };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
